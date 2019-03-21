9cf2bbab1e17eaa14195902a3a4073710842900d
dipankar61 <dipankar61@gmail.com>
2019-03-21 00:49:17 -0400

    Capsule task manager back end project structure

---
+++ TaskManager.Api/Controllers/TaskManagerController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TaskManager.Api.Controllers
+{
+    public class TaskManagerController : ApiController
+    {
+    }
+}
